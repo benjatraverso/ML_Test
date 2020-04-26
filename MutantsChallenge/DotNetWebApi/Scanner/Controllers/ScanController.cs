eec7b0feaaca72dfbeba4095de4b6f2cff2e808e
Benjamin Traverso <benjamin.traverso@amtek.com.ar>
2020-04-26 17:50:22 -0300

    master: NetCore WebApi initial commit. Core will be in "Scan controller"

---
+++ MutantsChallenge/DotNetWebApi/Scanner/Controllers/ScanController.cs
@@ -0,0 +1,42 @@
+﻿namespace Scanner.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using System.Collections.Generic;
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ScanController : ControllerBase
+    {
+        // GET: api/Scan
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Scan/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Scan
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/Scan/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
