4a745c7d17db35b5fc960e9d46db696200836379
Benjamin Traverso <benjamin.traverso@amtek.com.ar>
2020-05-01 18:40:47 -0300

    remodel: return response code

--- MutantsChallenge/DotNetWebApi/Scanner/Controllers/ScanController.cs
+++ MutantsChallenge/DotNetWebApi/Scanner/Controllers/ScanController.cs
@@ -1,18 +1,31 @@
 ﻿namespace Scanner.Controllers
 {
+    using Microsoft.AspNetCore.Http;
     using Microsoft.AspNetCore.Mvc;
     using Scanner.Models;
     using System.Collections.Generic;
+    using System.Net;
+    using System.Net.Http;
 
     public class ScanController : Controller
     {
         [HttpPost]
         [AcceptVerbs("Post")]
         [Route("api/Scan/Mutant")]
-        public dynamic Mutant([FromBody] Dictionary<string, object> dna)
+        public HttpResponseMessage Mutant([FromBody] Dictionary<string, object> dna)
         {
             var cadenas = DNADataParser.ParseDNAData(dna);
-            return Scan.IsMutant(cadenas);
+            var response = new HttpResponseMessage();
+            if (Scan.IsMutant(cadenas))
+            {
+                response.StatusCode = HttpStatusCode.OK;
+            }
+            else
+            {
+                response.StatusCode = HttpStatusCode.Forbidden;
+            }
+
+            return response;
         }
     }
 }
