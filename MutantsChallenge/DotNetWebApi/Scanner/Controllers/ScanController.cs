dbddb6cfad091a8054ef717b0a51d1a30a24cb5f
Benjamin Traverso <benjamin.traverso@amtek.com.ar>
2020-05-01 19:18:08 -0300

    remodel: se agrega endpoint de stats

--- MutantsChallenge/DotNetWebApi/Scanner/Controllers/ScanController.cs
+++ MutantsChallenge/DotNetWebApi/Scanner/Controllers/ScanController.cs
@@ -1,7 +1,7 @@
 ﻿namespace Scanner.Controllers
 {
-    using Microsoft.AspNetCore.Http;
     using Microsoft.AspNetCore.Mvc;
+    using Newtonsoft.Json;
     using Scanner.Models;
     using System.Collections.Generic;
     using System.Net;
@@ -14,18 +14,30 @@
         [Route("api/Scan/Mutant")]
         public HttpResponseMessage Mutant([FromBody] Dictionary<string, object> dna)
         {
-            var cadenas = DNADataParser.ParseDNAData(dna);
+            var cadenas = JsonConvert.DeserializeObject<List<string>>(dna["dna"].ToString());
             var response = new HttpResponseMessage();
+            DB.Kind type;
             if (Scan.IsMutant(cadenas))
             {
                 response.StatusCode = HttpStatusCode.OK;
+                type = DB.Kind.mutant;
             }
             else
             {
                 response.StatusCode = HttpStatusCode.Forbidden;
+                type = DB.Kind.human;
             }
 
+            DB.StoreResult(string.Join(",", cadenas), type);
             return response;
+        }
+
+        [HttpPost]
+        [AcceptVerbs("Get")]
+        [Route("api/Scan/Stats")]
+        public dynamic Stats()
+        {
+            return JsonConvert.SerializeObject(DB.GetStats());
         }
     }
 }
---
+++ MutantsChallenge/DotNetWebApi/Scanner/Models/DB.cs
@@ -0,0 +1,58 @@
+﻿namespace Scanner.Models
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class DB
+    {
+        internal enum Kind
+        {
+            human = 0,
+            mutant
+        }
+
+        internal static Dictionary<string, Kind> db = new Dictionary<string, Kind>();
+
+        internal static void StoreResult(string dna, Kind result)
+        {
+            var value = new Dictionary<string, Kind>();
+            if (!db.TryGetValue(dna, out _))
+            {
+                db.TryAdd(dna, result);
+            }
+        }
+
+        internal static DBStats GetStats()
+        {
+            var humans = db.Values.Where(x => x == Kind.human).Count();
+            var mutants = db.Values.Where(x => x == Kind.mutant).Count();
+            var res = new DBStats(mutants, humans);
+            return res;
+        }
+    }
+
+    public class DBStats
+    {
+        public DBStats(int mut, int hum)
+        {
+            count_mutant_dna = mut;
+            count_human_dna = hum;
+            if (hum == 0 && mut == 0)
+            {
+                ratio = 0;
+            }
+            if (hum == 0 && mut != 0)
+            {
+                ratio = 1;
+            }
+            else
+            {
+                ratio = hum == 0 ? 0 : mut / hum;
+            }
+        }
+
+        public int count_mutant_dna { get; set; }
+        public int count_human_dna { get; set; }
+        public decimal ratio { get; set; }
+    }
+}
